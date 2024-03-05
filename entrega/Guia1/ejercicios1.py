a0f60fecfb74981ce1471ddfcad21e1ede2435ae
Diego17A <161627119+Diego17A@users.noreply.github.com>
2024-03-05 19:50:06 +0000

    ultimo3

--- entrega/Guia1/ejercicios1.py
+++ entrega/Guia1/ejercicios1.py
@@ -1,3 +1,4 @@
+#01/03/24
 print ("Las maquinas me sorprenden con mucha frecuencia")
 print ()
 print ("tercer clase de python")
@@ -5,3 +6,7 @@ print ("23",23)
 print ("una compuradora puede ser llamada\"inteligente\" si logra engañar a una persona haciednole creer que es un humano")
 print ('una computadora puede ser llamada "inteligente" si logra engañar a una persona haciendole creer que es un humano')
 print ('Diego','Amuchastegui','33')
+# 05/03/24 
+print('Diego','Amuchastegui','33',sep='_')
+print('Calle','\t','numero','\t','codigo postal')
+print('Calle','\1','numero','\1','codigo postal')   
--- entrega/Principal.py
+++ entrega/Principal.py
@@ -6,3 +6,55 @@ print ("es viernes","y el cuerpo lo sabe",sep="+")
 print ("modernizacion","2024",end="*")
 print ("2024")
 print (type (40))
+
+edad=15
+print(f'su edad es: {edad}')
+num1=14
+num2=2
+
+#print
+texto='Es un tiTuLo'
+print(texto.title())
+print(texto)
+texto=texto.title()
+print(texto)
+
+#upper lower
+print(texto.upper())
+print(texto.lower())
+#replace
+print(texto.replace('',':)'))
+print(len(texto))
+#sentencia if
+edad=int(input('Ingese sus edad:'))
+if(edad>=18):
+    print('ud. debe votar') 
+else:
+    print('es menor de edad')
+print('indepediente')
+#ejemplo
+calificacion=int(input('ingrese calificacion'))
+if calificacion >=90:
+    print('excelente')
+else:
+    if calificacion >=80:
+        print('muy bien')
+    else:
+        if calificacion >=70:
+            print ('bien')
+        else:
+            print('insuficiente')
+#sentencia elif
+calificacion=int(input('ingrese calificacion'))
+if calificacion >=90:
+    print('excelente')
+elif calificacion >=80:
+    print('muy bien')
+elif calificacion >=70:
+    print('bien')
+else:
+    print('insufieciente')
+    
+# match 
+
+            
