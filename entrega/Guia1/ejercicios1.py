72eab57861e63a611c373f85cfe91972b0bff089
Diego17A <161627119+Diego17A@users.noreply.github.com>
2024-03-12 17:56:16 +0000

    uñtimo4

--- entrega/Guia1/ejercicios1.py
+++ entrega/Guia1/ejercicios1.py
@@ -1,4 +1,5 @@
 #01/03/24
+#ejercicio 1
 print ("Las maquinas me sorprenden con mucha frecuencia")
 print ()
 print ("tercer clase de python")
@@ -9,4 +10,49 @@ print ('Diego','Amuchastegui','33')
 # 05/03/24 
 print('Diego','Amuchastegui','33',sep='_')
 print('Calle','\t','numero','\t','codigo postal')
-print('Calle','\1','numero','\1','codigo postal')   
+print('Calle','\n''numero','\n''codigo postal')   
+#12/03/24
+print ('Feliz','\n''\t''Primavera','\n''\t\t\t''2024')
+print ('Solo podemos ver poco del futuro',end=', ')
+print ('pero lo suficiente para darnos cuenta de que hay mucho que hacer')
+print ('     *')
+print ('    * *')
+print ('   *   *')
+print ('  *     *')
+print (' ***   ***')
+print ('   *   *')
+print ('   *   *')
+print ('   *****')
+#ejercicio 2
+print (5+7) #suma
+print (0-9) #resta
+print (2*45)
+print (3/2)
+print (6**2)
+print (3//2)
+x= 2
+print (x**2 + 6*x+9)
+#ejercicio 3
+nombre= Diego
+apellido= amuchastegui
+edad= 33
+altura= 1.77
+numerodevuelo= 894
+tempambiente= 25
+salariomensual= 1000000
+juegoterminado= finalizado
+par= true
+#ejercicio 4
+nombre= Diego #str
+apellido= amuchastegui #str
+edad= 33 #int   
+altura= 1.77 #floot
+numerodevuelo= 894 #int
+tempambiente= 25 #int
+salariomensual= 1000000 #int
+juegoterminado= finalizado #str
+par= true #booleano
+#ejercicio 5
+#a
+
+print(input())
