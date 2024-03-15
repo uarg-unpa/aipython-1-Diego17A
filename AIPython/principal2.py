e1f1219b784717d388964d2505a42d1973adab74
Diego17A <161627119+Diego17A@users.noreply.github.com>
2024-03-15 19:48:03 +0000

    ultimo 9

--- AIPython/principal2.py
+++ AIPython/principal2.py
@@ -4,3 +4,19 @@ num=40
 print(id(num))
 num=22
 print(id(num))
+
+num=2
+def multiplicacion (x):
+    num=3
+    return x*num
+print (multiplicacion (8))
+
+def mutables(lista):
+    lista[2]=35
+
+mis_numeros=[1,2,3]
+print('antes de invocar a la funcion')
+print (mis_numeros)
+mutables(mis_numeros)
+print ('despues de llamar a la funcion mutables')
+print (mis_numeros)
